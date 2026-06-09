471d251aa3db5480fcdd43601ac46a688f6e429e
Eduardo De La Cruz <eduardo.delacruz42@uabc.edu.mx>
2026-06-09 13:56:27 -0700

    Define types for server

---
+++ frontEnd/src/types/serverTypes.ts
@@ -0,0 +1,22 @@
+
+// Define types of server
+export interface RuleItem {
+  title: string;
+  description: string;
+  example?: string;
+}
+
+export interface RuleSection {
+  title: string;
+  rules: RuleItem[];
+}
+
+export interface RPServer {
+  id: string;
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string;
+  discordLink: string;
+  sections: RuleSection[];
+}
