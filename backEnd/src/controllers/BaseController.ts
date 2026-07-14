bb57041578c1d69493b0eb44d182bc5b035ed457
Eduardo De La Cruz <eduardo.delacruz42@uabc.edu.mx>
2026-07-14 12:41:31 -0700

    Lint baseController

--- backEnd/src/controllers/BaseController.ts
+++ backEnd/src/controllers/BaseController.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
 
 export class BaseController {
-  protected sendSuccess(res: Response, data: any, statusCode: number = 200): void {
+  protected sendSuccess(res: Response, data: object, statusCode: number = 200): void {
     res.status(statusCode).json({
       success: true,
       data
