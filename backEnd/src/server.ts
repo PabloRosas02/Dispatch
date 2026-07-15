6102565d63ae73d765ff6982029d2556340c2253
Eduardo De La Cruz <eduardo.delacruz42@uabc.edu.mx>
2026-07-15 12:31:33 -0700

    Min cache update

--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -8,6 +8,7 @@ const PORT = process.env.PORT ?? 3000;
 
 // --- MIDDLEWARES CENTRALES ---
 app.use(cors());
+app.set('etag', 'strong')
 // MODIFICACIÓN: Se incrementa el límite a 50mb para soportar payloads grandes de Base64
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
